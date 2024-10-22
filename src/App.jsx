400a0c35f0da13068c9c4bc121a11cfe300f5db1
forhad30 <forhadchariani30@gmail.com>
2024-10-22 07:49:35 +0600

    react knowled cafe frits commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <h1 className='text-white font-bold text-4xl bg-green-600'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
